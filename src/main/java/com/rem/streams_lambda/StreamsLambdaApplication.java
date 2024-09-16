5b2f4ccdf44ece12663374dcd3816d5cbb7c2d0d
aladagramazan <ramazan.aladag@yaani.com>
2024-09-16 12:21:47 +0300

    init commit

---
+++ src/main/java/com/rem/streams_lambda/StreamsLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.rem.streams_lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rem/streams_lambda/StreamsLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rem.streams_lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
